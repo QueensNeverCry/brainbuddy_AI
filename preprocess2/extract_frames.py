5d4ca8b104c1ab60d8848222168cc10efa6af123
Xeoyeon <mariajang16@ewhain.net>
2025-07-15 17:02:36 +0900

    AIHub data labeling

--- preprocess2/extract_frames.py
+++ preprocess2/extract_frames.py
@@ -65,7 +65,7 @@ def extract_frames(video_path, output_base_path, segment_duration=10, target_fps
     print("전체 작업 완료")
         
 if __name__ == "__main__":
-    video_path = "C:/GitHub/brainbuddy/AIHub/train/60/02.mp4"
-    output_base_path = "./AIHub_frames/train/60_02"
+    video_path = "C:/GitHub/brainbuddy/AIHub/train/60/03.mp4"
+    output_base_path = "./AIHub_frames/train/60_03"
 
     extract_frames(video_path, output_base_path)
