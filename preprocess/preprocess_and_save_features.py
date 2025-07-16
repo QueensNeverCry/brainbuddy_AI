80e70957eb7536d30725cd2f7584cf48bce49619
Xeoyeon <mariajang16@ewhain.net>
2025-07-16 14:42:21 +0900

    commit for merge

--- preprocess/preprocess_and_save_features.py
+++ preprocess/preprocess_and_save_features.py
@@ -13,7 +13,7 @@ def preprocess_dataset(dataset_link, save_dir, T=10, device=None):
     face_mesh = mp.solutions.face_mesh.FaceMesh(
         static_image_mode=True,
         max_num_faces=1,
-        refine_landmarks=True,
+        refine_landmarks=True, 
         min_detection_confidence=0.4,
         min_tracking_confidence=0.4
     )
