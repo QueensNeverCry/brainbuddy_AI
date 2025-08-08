833098404eb3cbb77f8ccce50bba45468c9ee277
Xeoyeon <mariajang16@ewhain.net>
2025-08-08 10:37:00 +0900

    add train6 and late fusion

--- preprocess2/labeling.py
+++ preprocess2/labeling.py
@@ -3,10 +3,10 @@ import json
 import pickle
 
 label_base_dir = os.path.normpath(
-    r"C:/Users/user/Downloads/109.학습태도 및 성향 관찰 데이터/3.개방데이터/1.데이터/Validation/02.라벨링데이터/VL_20_01"
+    r"C:/Users/user/Downloads/109.학습태도 및 성향 관찰 데이터/3.개방데이터/1.데이터/Training/02.라벨링데이터/TL_20_03"
 )
-train_base_dir = os.path.normpath(r"C:/AIhub_frames/valid")
-output_pickle_path = os.path.normpath("pickle_labels/valid/20_01.pkl")
+train_base_dir = os.path.normpath(r"C:/AIhub_frames/train")
+output_pickle_path = os.path.normpath("pickle_labels/train/20_03.pkl")
 
 label_texts = set()
 label_map = {
